e671d4e4363a37859759fda64b35ef272ee3ff5c
Akshaypro1 <166587687+Akshaypro1@users.noreply.github.com>
2025-06-24 15:59:38 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 // config/db.js
 module.exports = 
-{    host: 'p3nlmysql15plsk.secureserver.net',    
-    user: 'testshatam',    
-    password: 'H#74zs5x0',    
-    database: 'testshatam',    
+{    host: 'xxx',    
+    user: 'xx',    
+    password: 'xxx',    
+    database: 'xxx',    
     waitForConnections: true,    
     connectionLimit: 10,    
     queueLimit: 0  
